e9f99c08d70ec7082dbad32fa0887ffdce0eab08
Edmundas <senelis@gmail.com>
2022-10-30 13:47:01 +0200

    created function

--- Task 7/script.js
+++ Task 7/script.js
@@ -5,8 +5,17 @@ Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
 const audi = {
-  make: 'audi',
-  model: 'A6',
+  make: "audi",
+  model: "A6",
   year: 2005,
-  color: 'white',
+  color: "white",
 };
+
+function showObjectKeys(object) {
+  let result = [];
+  for (const key in object) {
+    result.push(key);
+  }
+  return result;
+}
+console.log(showObjectKeys(audi));
