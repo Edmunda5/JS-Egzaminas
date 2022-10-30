e7164e4ca2bd4261047cdee7bdaeb86c5442905e
Edmundas <senelis@gmail.com>
2022-10-30 13:43:45 +0200

    Created click lisener

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,13 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+document.getElementById("submit-btn").onclick = (e) => {
+  console.log("click");
+  let value = parseFloat(document.getElementById("search").value);
+  let result = `lb: ${value * 2.2046}, g: ${value / 0.001}, oz: ${
+    value * 35.274
+  }`;
+  document.getElementById("output").innerHTML = result;
+  return false;
+};
